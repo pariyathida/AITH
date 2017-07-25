786aab80ab36c1f83f8ed2a51685bbe77be54422
DESKTOP-QKKUBVT\pariy <pariyathida@gmail.com>
2017-07-25 23:29:01 +0700

    Init commit

---
+++ index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import Game from './game.js';
+AppRegistry.registerComponent('Game', () => Game);
